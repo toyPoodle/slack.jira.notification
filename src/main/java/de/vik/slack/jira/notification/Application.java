7b77893fdc12686ed26ae37ef92aabd97f6112a3
toyPoodle <toyPoodle@users.noreply.github.com>
2018-01-30 00:19:15 +0100

    proxying args to SpringApplication to be able to use "--debug"

--- src/main/java/de/vik/slack/jira/notification/Application.java
+++ src/main/java/de/vik/slack/jira/notification/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.SpringApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
